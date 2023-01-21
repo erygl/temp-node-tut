72dc93e094130c480ff5c53554dd96767162e5b3
eray.gul@windowslive.com <eray.gul@windowslive.com>
2023-01-21 17:57:05 +0300

    second commit

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run fast')
+    //process statys alive unless
+    // Kill process CONTROL + C
+    // unexpected error
